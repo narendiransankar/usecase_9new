22817aa72717a4391fa0876d6de702d2c76eabf2
ArunAK4 <arunkumara619@gmail.com>
2025-07-17 21:20:08 +0530

    uc7

--- usecase-7/modules/apigateway/main.tf
+++ usecase-7/modules/apigateway/main.tf
@@ -1,28 +1,41 @@
-resource "aws_api_gateway_rest_api" "this" {
+resource "aws_api_gateway_rest_api" "api" {
   name        = var.api_name
   description = "API Gateway with Lambda Proxy Integration"
 }
  
+resource "aws_api_gateway_resource" "root" {
+rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = ""
+}
+ 
 resource "aws_api_gateway_method" "get" {
-rest_api_id = aws_api_gateway_rest_api.this.id
-resource_id = aws_api_gateway_rest_api.this.id
+rest_api_id = aws_api_gateway_rest_api.api.id
+resource_id = aws_api_gateway_resource.root.id
   http_method   = "GET"
   authorization = "NONE"
 }
  
 resource "aws_api_gateway_integration" "lambda" {
-rest_api_id = aws_api_gateway_rest_api.this.id
-resource_id = aws_api_gateway_resource.proxy.id
+rest_api_id = aws_api_gateway_rest_api.api.id
+resource_id = aws_api_gateway_resource.root.id
   http_method = aws_api_gateway_method.get.http_method
  
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = var.lambda_invoke_arn
 }
-
  
-resource "aws_api_gateway_deployment" "this" {
-  depends_on = [aws_api_gateway_integration.lambda]
-rest_api_id = aws_api_gateway_rest_api.this.id
+resource "aws_lambda_permission" "apigw_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_function_name
+principal = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+ 
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda]
+rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = var.stage_name
 }
