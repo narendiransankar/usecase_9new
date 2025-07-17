0118f959c1785dfccc86be02ccd2fdc0b1b91d65
ArunAK4 <arunkumara619@gmail.com>
2025-07-17 19:50:35 +0530

    uc7

---
+++ usecase-7/main.tf
@@ -0,0 +1,7 @@
+module "lambda" {
+  source = "./modules/lambda"
+  lambda_name = var.lambda_name
+  image_uri = var.image
+  memory_size = 256
+  timeout = 60
+}
---
+++ usecase-7/modules/lambda/variable.tf
@@ -0,0 +1,9 @@
+variable "lambda_name" {
+    type = string
+    description = "Lambda name"
+}
+
+variable "image_uri" {
+    type = string
+    description = "ECR repo URI"
+}
