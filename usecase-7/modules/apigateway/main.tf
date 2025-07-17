9fc9a370c97c09e5c92c57b905333d69ba05c990
ArunAK4 <arunkumara619@gmail.com>
2025-07-17 21:32:42 +0530

    uc7

--- usecase-7/modules/apigateway/main.tf
+++ usecase-7/modules/apigateway/main.tf
@@ -3,15 +3,9 @@ resource "aws_api_gateway_rest_api" "api" {
   description = "API Gateway with Lambda Proxy Integration"
 }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
-  path_part   = ""
-}
-
 resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.root.id
+  resource_id   = aws_api_gateway_resource.api.root_resource_id
   http_method   = "GET"
   authorization = "NONE"
 }
