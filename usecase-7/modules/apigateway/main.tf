04e51bbd293d552a8be6d64c1f4f891c42d13ff2
ArunAK4 <arunkumara619@gmail.com>
2025-07-17 21:34:42 +0530

    uc7

--- usecase-7/modules/apigateway/main.tf
+++ usecase-7/modules/apigateway/main.tf
@@ -12,7 +12,7 @@ resource "aws_api_gateway_method" "get" {
 
 resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.root.id
+  resource_id = aws_api_gateway_rest_api.api.root_resource_id
   http_method = aws_api_gateway_method.get.http_method
 
   integration_http_method = "POST"
