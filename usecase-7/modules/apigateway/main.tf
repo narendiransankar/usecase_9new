b146d0c24cebaf7120e71fd753fcf6b724c37f2d
ArunAK4 <arunkumara619@gmail.com>
2025-07-17 21:33:37 +0530

    uc7

--- usecase-7/modules/apigateway/main.tf
+++ usecase-7/modules/apigateway/main.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "api" {
 
 resource "aws_api_gateway_method" "get" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.api.root_resource_id
+  resource_id   = aws_api_gateway_rest_api.api.root_resource_id
   http_method   = "GET"
   authorization = "NONE"
 }
