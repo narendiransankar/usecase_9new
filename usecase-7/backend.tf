77f38bc7e8a2f98e2f960a43b440ed6cf4893b69
ArunAK4 <arunkumara619@gmail.com>
2025-07-17 19:56:34 +0530

    uc7

--- usecase-7/backend.tf
+++ usecase-7/backend.tf
@@ -1,12 +1,9 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  required_version = ">=1.0.0"
+  backend "s3" {
+    bucket       = "terraform-usecases-batch6"
+    key          = "usecase7/terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
   }
-}
-
-provider "aws" {
-  region = "ap-south-1"
 }
