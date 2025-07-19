a013181bfd9470a78822cb6ad8b9ece48cf5cf2f
ArunAK4 <arunkumara619@gmail.com>
2025-07-19 12:47:04 +0530

    initial uc8

--- usecase-8/backend.tf
+++ usecase-8/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-usecases-batch6"
     key          = "usecase8/terraform.tfstate"
-    region       = "ap-south-1"
+    region       = "eu-west-1"
     use_lockfile = true
   }
 }
