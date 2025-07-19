3e36fc65cd9255778f14e49567a3383dccc0007b
ArunAK4 <arunkumara619@gmail.com>
2025-07-19 15:55:51 +0530

    initial uc8

--- usecase-8/variable.tf
+++ usecase-8/variable.tf
@@ -35,18 +35,6 @@ variable "cluster_name" {
     type = string
 }
 
-variable "container_name" {
-    type = string
-}
-
-variable "container_image" {
-    type = string
-}
-
-variable "container_port" {
-    type = number
-}
-
 variable "execution_role_arn" {
     type = string
 }
