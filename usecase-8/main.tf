86c6c7be6c7bf3d60e10e5101638e998f0ec6d57
ArunAK4 <arunkumara619@gmail.com>
2025-07-19 14:15:18 +0530

    initial uc8

--- usecase-8/main.tf
+++ usecase-8/main.tf
@@ -19,3 +19,12 @@ module "sg" {
     alb_sg_name = var.alb_sg_name
     sg_name = var.sg_name
 }
+
+
+module "alb" {
+  source         = "./modules/alb"
+  name           = "uc8-alb"
+  vpc_id         = module.vpc.vpc_id
+  public_subnets = module.vpc.public_subnet_ids
+  alb_sg_id      = module.security_group.alb_sg_id
+}
