b7a180379165b69bfb8f7e8d5a35f56c3161406e
ArunAK4 <arunkumara619@gmail.com>
2025-07-19 16:24:34 +0530

    initial uc8

--- usecase-8/modules/ecs/variable.tf
+++ usecase-8/modules/ecs/variable.tf
@@ -17,3 +17,21 @@ variable "container_port" {
 variable "execution_role_arn" {
     type = string
 }
+
+variable "subnets" {
+    type = list(string)
+}
+
+variable "security_groups" {
+    type = list(string)
+}
+
+variable "services" {
+    type = map(object({
+        image = string
+        cpu = string
+        memory = string
+        container_port = string
+        target_group_arn = string
+    }))
+}
