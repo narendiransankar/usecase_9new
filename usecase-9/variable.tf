4a7126d438d7c3a0d0b127fea05b38d2e47b6bf2
ArunAK4 <arunkumara619@gmail.com>
2025-07-24 11:41:24 +0530

    added vpc module

--- usecase-9/variable.tf
+++ usecase-9/variable.tf
@@ -8,3 +8,23 @@ variable "node_role_arn" {}
 variable "subnet_ids" {
   type = list(string)
 }
+
+variable "vpc_cidr" {
+    type = string
+}
+
+variable "vpc_name" {
+    type = string
+}
+
+variable "public_subnets" {
+    type = list(string)
+}
+
+variable "private_subnets" {
+    type = list(string)
+}
+
+variable "azs" {
+    type = list(string)
+}
