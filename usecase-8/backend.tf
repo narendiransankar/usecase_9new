d0f8192e01b7465dbe02e8c564baf709ead8641c
narendiransankar <narendiransankar1699@gmail.com>
2025-07-24 13:38:56 +0530

    Delete usecase-8 directory

--- usecase-8/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-usecases-batch6"
-    key          = "usecase8/terraform.tfstate"
-    region       = "eu-west-1"
-    use_lockfile = true
-  }
-}
