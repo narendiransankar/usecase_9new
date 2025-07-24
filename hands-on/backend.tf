f89bf95940939062078f52b4ecb55f808514db5c
narendiransankar <narendiransankar1699@gmail.com>
2025-07-24 13:37:38 +0530

    Delete hands-on directory

--- hands-on/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-usecases-batch6"
-    key          = "hands-on-1/terraform.tfstate"
-    region       = "eu-west-1"
-    use_lockfile = true
-  }
-}
