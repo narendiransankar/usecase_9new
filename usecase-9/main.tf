94b38310553e67f1f41bddff8758a0727ee4d785
ArunAK4 <arunkumara619@gmail.com>
2025-07-24 11:32:22 +0530

    added vpc module

--- usecase-9/main.tf
+++ usecase-9/main.tf
@@ -6,7 +6,7 @@ module "eks_cluster" {
   source             = "./modules/cluster"
   cluster_name       = var.cluster_name
   cluster_role_arn   = var.cluster_role_arn
-  subnet_ids         = var.subnet_ids
+  subnet_ids         = module.vpc.private_subnet_ids
   kubernetes_version = "1.29"
 }
 
@@ -15,9 +15,9 @@ module "eks_nodegroup" {
   cluster_name    = var.cluster_name
   node_group_name = "default-node-group"
   node_role_arn   = var.node_role_arn
-  subnet_ids      = var.subnet_ids
-  desired_size    = 2
+  subnet_ids      = module.vpc.private_subnet_ids
+  desired_size    = 1
   min_size        = 1
-  max_size        = 3
+  max_size        = 1
   instance_type   = "t3.medium"
 }
